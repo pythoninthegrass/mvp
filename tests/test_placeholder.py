5f313cbe01cf22d47169ec9e70ff2eab6b616338
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-09-20 14:31:27 -0500

    test: add boilerplate test

---
+++ tests/test_placeholder.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_placeholder(capfd):
+    """
+    Boilerplate test to allow for GitHub Actions to run.
+
+    TODO: write actual tests
+    """
+    print("This is a placeholder test running in CI/CD pipeline")
+    assert True
+    out, err = capfd.readouterr()
+    assert "placeholder test" in out, "Expected output not found"
